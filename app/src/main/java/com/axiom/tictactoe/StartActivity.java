689de1f177217ed53a24c1d93221cc78cdb2a7fc
axiom0 <infonewbar21@gmail.com>
2020-09-08 02:45:36 +0900

    Implement spinner to choose player's side(red or blue).

--- app/src/main/java/com/axiom/tictactoe/StartActivity.java
+++ app/src/main/java/com/axiom/tictactoe/StartActivity.java
@@ -5,13 +5,36 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Spinner;
 
 public class StartActivity extends AppCompatActivity {
+    private String playerTurnStr;
+    private String[] sideArray = getResources().getStringArray(R.array.side_array);
+    private int playerTurn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
+        Spinner spinner = findViewById(R.id.spinner);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                playerTurnStr = (String)adapterView.getItemAtPosition(i);
+                if(playerTurnStr.equals(sideArray[0])){
+                    playerTurn = TicTacToe.RED;
+                }else if(playerTurnStr.equals(sideArray[1])){
+                    playerTurn = TicTacToe.BLUE;
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
     }
 
     public void startOffline(View view){
@@ -20,6 +43,7 @@ public class StartActivity extends AppCompatActivity {
     }
     public void startCPU(View view){
         Intent intent = new Intent(getApplicationContext(), CpuActivity.class);
+        intent.putExtra("PL_TURN", playerTurn);
         startActivity(intent);
     }
     public void startOnline(View view){
