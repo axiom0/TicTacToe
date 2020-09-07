d3c5941383514782e8ee2778ce8305d6a8b4cc3a
axiom0 <infonewbar21@gmail.com>
2020-09-08 02:49:22 +0900

    Implement spinner to choose player's side(red or blue).

--- app/src/main/java/com/axiom/tictactoe/CpuActivity.java
+++ app/src/main/java/com/axiom/tictactoe/CpuActivity.java
@@ -5,17 +5,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class CpuActivity extends Board {
+    private int playerTurn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        playerTurn = getIntent().getIntExtra("PL_NAME", TicTacToe.RED);
     }
 
 
     @Override
     protected void onClick(int ID,int position){
         super.onClick(ID,position);
+
 
     }
 }
