85a8ed9498a0240ba0b8a998759d4898c76f59b2
axiom0 <infonewbar21@gmail.com>
2020-09-07 13:49:43 +0900

    Implemented Start Menu.

---
+++ app/src/main/java/com/axiom/tictactoe/StartActivity.java
@@ -0,0 +1,29 @@
+package com.axiom.tictactoe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class StartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+    }
+
+    public void startOffline(View view){
+        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(intent);
+    }
+    public void startCPU(View view){
+        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(intent);
+    }
+    public void startOnline(View view){
+        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(intent);
+    }
+}
